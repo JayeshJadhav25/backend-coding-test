d766ef333cce7dd47ad740186671bc40ab72156a
JayeshJadhav25 <jayeshjadhav684@gmail.com>
2023-01-02 23:12:32 +0530

    Implemented Swagger Api Documentation

--- index.js
+++ index.js
@@ -12,10 +12,36 @@ const db = new sqlite3.Database(':memory:');
 
 const buildSchemas = require('./src/schemas');
 
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+    definition: {
+        openapi:'3.0.0',
+        info : {
+            title:'Backend Coding Test',
+            description:'tets',
+            version:'1.0.0'
+        },
+        servers:[
+            {
+                url:'http://localhost:8010/'
+            }
+        ]
+    },
+    apis:['./src/app.js']
+}
+
+const swaggerSpec = swaggerJSDoc(options);
+
+
+
 db.serialize(() => {
     buildSchemas(db);
 
     const app = require('./src/app')(db);
+
+    app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerSpec));
 
     app.listen(port, () => console.log(`App started and listening on port ${port}`));
 });
--- src/app.js
+++ src/app.js
@@ -6,9 +6,105 @@ const app = express();
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
+
+
 module.exports = (db) => {
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Ride:
+ *       type: object
+ *       required:
+ *         - start_lat
+ *         - start_long
+ *         - end_lat
+ *         - end_long
+ *         - rider_name
+ *         - driver_name
+ *         - driver_vehicle
+ *       properties:
+ *         start_lat:
+ *           type: number
+ *           minimum: -90,
+ *           maximum: 90,
+ *           description: Starting Latitude
+ *         start_long:
+ *           type: number
+ *           minimum: -180,
+ *           maximum: 180,
+ *           description: Starting Longitude
+ *         end_lat:
+ *           type: number
+ *           minimum: -90,
+ *           maximum: 90,
+ *           description: Ending Latitude
+ *         end_long:
+ *           type: number
+ *           minimum: -180,
+ *           maximum: 180,
+ *           description: Ending Longitude
+ *         rider_name:
+ *           type: string
+ *           description: Rider Name
+ *         driver_name:
+ *           type: string
+ *           description: Driver Name
+ *         driver_vehicle:
+ *           type: string
+ *           description: Driver Vehicle
+ *       example:
+ *         start_lat: 40
+ *         start_long: 30 
+ *         end_lat: 30
+ *         end_long: 40
+ *         rider_name: Jayesh
+ *         driver_name: Mahesh
+ *         driver_vehicle: Car
+ */
+
+ /**
+  * @swagger
+  * tags:
+  *   name: Rides
+  *   description: The Rides managing API
+  */
+
+
+ /**
+ * @swagger
+ * /health:
+ *   get:
+ *     summary: This api check the health
+ *     responses:
+ *       200:
+ *         description: It will return healthy if server is properly running
+ */
+
     app.get('/health', (req, res) => res.send('Healthy'));
 
+    /**
+ * @swagger
+ * /rides:
+ *   post:
+ *     summary: Book a new ride
+ *     tags: [Rides]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Ride'
+ *     responses:
+ *       200:
+ *         description: The ride was successfully book
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *               $ref: '#/components/schemas/Ride'
+ */
     app.post('/rides', jsonParser, (req, res) => {
         const startLatitude = Number(req.body.start_lat);
         const startLongitude = Number(req.body.start_long);
@@ -76,6 +172,22 @@ module.exports = (db) => {
         });
     });
 
+/**
+ * @swagger
+ * /rides:
+ *   get:
+ *     summary: Returns the list of all the rides
+ *     tags: [Rides]
+ *     responses:
+ *       200:
+ *         description: The list of the Rides
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Ride'
+ */
     app.get('/rides', (req, res) => {
         db.all('SELECT * FROM Rides', function (err, rows) {
             if (err) {
@@ -95,6 +207,31 @@ module.exports = (db) => {
             res.send(rows);
         });
     });
+
+
+/**
+ * @swagger
+ * /rides/{id}:
+ *   get:
+ *     summary: Get the rides by id
+ *     tags: [Rides]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: number
+ *         required: true
+ *         description: The rides id
+ *     responses:
+ *       200:
+ *         description: The rides details by id
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                  $ref: '#/components/schemas/Ride'
+ */
 
     app.get('/rides/:id', (req, res) => {
         db.all(`SELECT * FROM Rides WHERE rideID='${req.params.id}'`, function (err, rows) {
